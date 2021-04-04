70f2f722a5bea89f8ad64395f43d82f8b2690134
bbeckford305 <bridgette.beckford@gmail.com>
2021-04-04 04:08:12 -0400

    Day 2 Save 1

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const store = require('./../store')
+
+const onSignUpSuccess = function () {
+  $('#message').text('Thank You For Registering')
+  $('#sign-up').trigger('reset')
+}
+
+const onSignUpFailure = function () {
+  $('#message').text('Registration Failure')
+}
+
+const onSignInSuccess = function (response) {
+  store.user = response.user
+  $('#message').text('Signed In')
+  $('#sign-in').trigger('reset')
+  $('#change-password').show()
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+  $('#signupForm').hide()
+  $('#signinForm').hide()
+  $('#passwordChangeForm').show()
+  $('#signoutButton').show()
+}
+
+const onSignInFailure = function () {
+  $('#message').text('Email/Password Combination Incorrect')
+}
+
+const onChangePasswordSuccess = function () {
+  $('#message').text('Your Password Has Been Changed')
+  $('#change-password').trigger('reset')
+}
+
+const onChangePasswordFailure = function () {
+  $('#message').text('Password Change Failed')
+}
+
+const onSignOutSuccess = function () {
+  store.user = null
+  $('#message').text('You Have Been Signed Out')
+  $('#change-password').hide()
+  $('#sign-up').show()
+  $('#sign-in').show()
+  $('#signupForm').show()
+  $('#signinForm').show()
+  $('#passwordChangeForm').hide()
+  $('#signoutButton').hide()
+}
+
+const onSignOutFailure = function () {
+  $('#message').text('Please Try Signing Out Again')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
